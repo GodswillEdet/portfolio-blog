16f16f94b835f37913fbf4704a482f43110a5048
Huynh <shinsusenju@aol.com>
2022-03-31 10:55:37 -0400

    Add more styling to contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,29 +5,37 @@ const Contact: React.FC = () => {
     <section aria-labelledby="contact" className={styles.contact}>
       <div className={styles.info}>
         <h2 id="contact">Leave me a message</h2>
-        <p>Let our conversation begin</p>
+        <p>Tell me what I can do for you</p>
         <h3></h3>
       </div>
       <div className={styles.form}>
         <form action="" method="POST">
-          <label htmlFor="name-input">Name</label>
-          <input type="text" name="name" id="name-input" />
-          <label htmlFor="email-input">Email</label>
-          <input type="email" name="email" id="email-input" />
-          <label htmlFor="message-input">Message</label>
-          <textarea name="message" id="message-input" />
-          <button type="submit" className={styles.submit}>
-            <span className={styles.submitText}>Send!</span>
-            <div className={styles.arrow}>
-              <Image
-                src="/icons/arrow-right.svg"
-                alt="Arrow icon"
-                width={18}
-                height={18}
-                layout="fixed"
-              />
-            </div>
-          </button>
+          <div className={styles.formSection}>
+            <label htmlFor="name-input">Name</label>
+            <input type="text" name="name" id="name-input" />
+          </div>
+          <div className={styles.formSection}>
+            <label htmlFor="email-input">Email</label>
+            <input type="email" name="email" id="email-input" />
+          </div>
+          <div className={styles.formSection}>
+            <label htmlFor="message-input">Message</label>
+            <textarea name="message" id="message-input" />
+          </div>
+          <div className={styles.formSection}>
+            <button type="submit" className={styles.submit}>
+              <span className={styles.submitText}>Send!</span>
+              <div className={styles.arrow}>
+                <Image
+                  src="/icons/arrow-right.svg"
+                  alt="Arrow icon"
+                  width={21}
+                  height={21}
+                  layout="fixed"
+                />
+              </div>
+            </button>
+          </div>
         </form>
       </div>
     </section>
