b5260936bac24ee857ebc94f6ac407f89bd1c261
bhuynhdev <shinsusenju@aol.com>
2022-08-13 16:39:15 -0400

    Add about image

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -10,26 +10,31 @@ export const About: React.FC = () => {
           <h2 id="about">About me</h2>
         </WatermarkHeader>
         <div className={styles.content}>
-          <div className={styles.intro}>
-            <p>Hi, I am Bao Huynh, a passionate web developer and a Computer Science student</p>
-            <p>
-              I enjoy making web applications that look great, feel fantastic, and perform well
-              using modern technogies.
-            </p>
+          <div className={styles.image}>
+            <img src="/images/about_me_2.jpg" alt="About me image" />
           </div>
-          <div className={styles.skills}>
-            <h3>Skills</h3>
-            <ul>
-              <li>Typescript</li>
-              <li>Python</li>
-              <li>C++</li>
-              <li>React.js</li>
-              <li>Express.js</li>
-              <li>HTML and CSS</li>
-              <li>MongoDB</li>
-              <li>Postgres SQL</li>
-              <li>Git/Github</li>
-            </ul>
+          <div className={styles.text}>
+            <div className={styles.intro}>
+              <p>Hi, I am Bao Huynh, a passionate web developer and a Computer Science student</p>
+              <p>
+                I enjoy making web applications that look great, feel fantastic, and perform well
+                using modern technogies.
+              </p>
+            </div>
+            <div className={styles.skills}>
+              <h3>Skills</h3>
+              <ul>
+                <li>Typescript</li>
+                <li>Python</li>
+                <li>C++</li>
+                <li>React.js</li>
+                <li>Express.js</li>
+                <li>HTML and CSS</li>
+                <li>MongoDB</li>
+                <li>Postgres SQL</li>
+                <li>Git/Github</li>
+              </ul>
+            </div>
           </div>
         </div>
       </Fade>
