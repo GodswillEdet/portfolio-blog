e471af5d266285c565ea17ad96704199e0177c29
Bao <huynhlbg@mail.uc.edu>
2022-08-30 17:26:38 -0400

    Add 'en' lang for html

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withYaml = require("next-plugin-yaml");
 
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 module.exports = withYaml(nextConfig);
